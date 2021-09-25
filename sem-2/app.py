d17dd6c1148f2ef4aaa4620cb0209f3883d97c2c
shmalens <zivaka00@gmail.com>
2021-09-25 10:07:19 +0300

    Закончил сем 2, добавил бд

--- sem-2/app.py
+++ sem-2/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect
+from pymysql import connect
 
 # template_folder - параметр конструктора Flask,
 # указывает директорию с шаблонами
@@ -55,6 +56,22 @@ def routing():
 
 # По умолчанию роут обрабатывает только GET
 # Можно явно указать, какие методы HTML он обрабатывает
+# @app.route("/form", methods=['GET', 'POST'])
+# def form():
+#     if request.method == 'GET':
+#         return render_template('forms.html')
+#     else:
+#         login = request.form.get('login', None)
+#         password = request.form.get('password', None)
+#
+#         if login and password:
+#             if login == 'kek' and password == 'lol':
+#                 return 'OK'
+#             else:
+#                 return 'Wrong login password'
+#
+#     return render_template('forms.html')
+
 @app.route("/form", methods=['GET', 'POST'])
 def form():
     if request.method == 'GET':
@@ -63,13 +80,18 @@ def form():
         login = request.form.get('login', None)
         password = request.form.get('password', None)
 
-        if login and password:
-            if login == 'kek' and password == 'lol':
-                return 'OK'
-            else:
-                return 'Wrong login password'
+        connection = connect(host='127.0.0.1', user='root', password='CoWeNt11', port=3306)
+        cursor = connection.cursor()
+        _sql_request = f"select user_id, name from sem2.users where login='{login}' and password='{password}'"
+        cursor.execute(_sql_request)
+        res = cursor.fetchall()
+        connection.close()
 
-    return render_template('forms.html')
+        if res:
+            return str(res)
+
+        else:
+            return "Error ocured"
 
 
 if __name__ == "__main__":
